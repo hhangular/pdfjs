3371d9f96df27708ccd1ae52f243e000f43fcc4b
Francois Achache <francois.achache@gmail.com>
2018-06-08 07:56:05 +0200

    expose api

--- projects/pdfjs-box2/src/public_api.ts
+++ projects/pdfjs-box2/src/public_api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/components';
 export * from './lib/pdfjs-box.module';
+export * from './lib/classes/pdfjs-control';
+export * from './lib/classes/pdfjs-objects';
+export * from './lib/classes/pdfapi';
