720535ed28d8e0f0f193f0a4799ad78d8270da82
Francois Achache <francois.achache@gmail.com>
2018-06-24 21:29:04 +0200

    miss class

---
+++ src/app/doc/sections/components/pdfjsview/api/api-pdfjsview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './api-pdfjsview.component.html',
+  styleUrls: ['./api-pdfjsview.component.css']
+})
+export class ApiPdfjsViewComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+}
