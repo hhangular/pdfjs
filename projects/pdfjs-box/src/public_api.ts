b862d354197539b510f9467fedca853a4db7c905
Francois Achache <francois.achache@gmail.com>
2018-06-25 11:22:08 +0200

    issues ngPAckage expose pertinent classes

--- projects/pdfjs-box/src/public_api.ts
+++ projects/pdfjs-box/src/public_api.ts
@@ -4,12 +4,7 @@
 
 export {PdfjsBoxModule} from './lib/pdfjs-box.module';
 export {PdfjsCommonComponent} from './lib/components/pdfjs-common.component';
-export {PdfjsConfig} from './lib/classes/pdfjs-objects';
+export {PdfjsConfig, PdfjsItem, PdfSource, ThumbnailDragMode, ThumbnailLayout, ViewFit} from './lib/classes/pdfjs-objects';
 export {PdfjsControl} from './lib/classes/pdfjs-control';
-export {PdfjsRemoveButtonComponent} from './lib/components/pdfjs-thumbnail/pdfjs-remove.button/pdfjs-remove-button.component';
-export {PdfjsThumbnailComponent} from './lib/components/pdfjs-thumbnail/pdfjs-thumbnail.component';
 export {PdfjsThumbnailsComponent} from './lib/components/pdfjs-thumbnails/pdfjs-thumbnails.component';
 export {PdfjsViewComponent} from './lib/components/pdfjs-view/pdfjs-view.component';
-export {ThumbnailDragService} from './lib/services/thumbnail-drag.service';
-export {KeysService} from './lib/services/keys.service';
-export {Pdfjs} from './lib/services/pdfjs.service';
