1aae76b847b125bca34223c95654896d707b31a3
hhfrancois-figaro <francois.achache@gmail.com>
2019-07-18 17:13:42 +0200

    try to publish only library 25

--- scripts/push-on-master.sh
+++ scripts/push-on-master.sh
@@ -2,8 +2,12 @@
 echo Push in current branch $TRAVIS_BRANCH
 git push https://$GITHUB_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_BRANCH
 
-echo Checkout master
-git checkout master
+echo get infos from remote
+git remote update
+git fetch
+
+echo Checkout origin/master
+git checkout origin/master
 
 echo Merge on master
 git merge master
