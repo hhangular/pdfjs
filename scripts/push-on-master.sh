9965a456cf0630e9efecc15f66e4b7d05f64b609
hhfrancois-figaro <francois.achache@gmail.com>
2019-07-18 18:11:06 +0200

    try to publish only library 27

--- scripts/push-on-master.sh
+++ scripts/push-on-master.sh
@@ -4,17 +4,19 @@ git push https://$GITHUB_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git $T
 
 echo Control remote
 git config --get remote.origin.fetch
+
+echo Set remote origin
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 
 echo get infos from remote
 git remote update
 git fetch
 
-echo Checkout origin/master
-git checkout origin/master
+echo Checkout master
+git checkout master
 
 echo Merge on master
-git merge master
+git merge $TRAVIS_BRANCH
 
 echo Push on master
 git push https://$GITHUB_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git master
